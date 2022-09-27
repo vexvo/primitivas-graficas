e03d9c7b568b57a6d78ac6eb63d4bba9c888aa71
vexvo <santiago.uribeg@autonoma.edu.co>
2022-09-27 16:21:25 -0500

    first commit

---
+++ src/primitivasgraficas/PrimitivasGraficas.java
@@ -0,0 +1,16 @@
+package primitivasgraficas;
+
+/**
+ *
+ * @author Santi
+ */
+public class PrimitivasGraficas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
